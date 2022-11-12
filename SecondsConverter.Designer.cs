936133dca6136e00d319e2f0e2266701e0e6500f
Jesse Dalton <jmdalton0@gmail.com>
2022-11-11 19:53:01 -0800

    added styles

---
+++ SecondsConverter.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Act4
+{
+    partial class SecondsConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numSecondsLabel = new System.Windows.Forms.Label();
+            this.numSecondsTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numSecondsLabel
+            // 
+            this.numSecondsLabel.AutoSize = true;
+            this.numSecondsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.numSecondsLabel.Location = new System.Drawing.Point(145, 157);
+            this.numSecondsLabel.Name = "numSecondsLabel";
+            this.numSecondsLabel.Size = new System.Drawing.Size(230, 20);
+            this.numSecondsLabel.TabIndex = 0;
+            this.numSecondsLabel.Text = "Enter the number of seconds:";
+            // 
+            // numSecondsTextBox
+            // 
+            this.numSecondsTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.numSecondsTextBox.Location = new System.Drawing.Point(464, 157);
+            this.numSecondsTextBox.Name = "numSecondsTextBox";
+            this.numSecondsTextBox.Size = new System.Drawing.Size(274, 22);
+            this.numSecondsTextBox.TabIndex = 1;
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.convertButton.Location = new System.Drawing.Point(464, 302);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(195, 53);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "CONVERT";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.resultLabel.Location = new System.Drawing.Point(97, 200);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(278, 20);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "The number of seconds amounts to:";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.resultTextBox.Location = new System.Drawing.Point(464, 200);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(274, 22);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // SecondsConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(910, 464);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.numSecondsTextBox);
+            this.Controls.Add(this.numSecondsLabel);
+            this.Name = "SecondsConverter";
+            this.Text = "Seconds Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numSecondsLabel;
+        private System.Windows.Forms.TextBox numSecondsTextBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
---
+++ SecondsConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act4
+{
+    public partial class SecondsConverter : Form
+    {
+        public SecondsConverter()
+        {
+            InitializeComponent();
+        }
+
+        // convert user-entered number of seconds to minutes, hours, or days
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            int numSeconds = 0;
+            String input = numSecondsTextBox.Text;
+
+            if (int.TryParse(input, out numSeconds) && numSeconds > 0)
+            {
+                if (numSeconds < 60)
+                {
+                    resultTextBox.Text = numSeconds.ToString() + " second(s)";
+                } 
+                else if (numSeconds < 3600)
+                {
+                    int numMinutes = numSeconds / 60;
+                    resultTextBox.Text = numMinutes.ToString() + " minute(s)";
+                }
+                else if (numSeconds < 86400)
+                {
+                    int numHours = numSeconds / 3600;
+                    resultTextBox.Text = numHours.ToString() + " hour(s)";
+                }
+                else
+                {
+                    int numDays = numSeconds / 86400;
+                    resultTextBox.Text = numDays.ToString() + " day(s)";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive integer.");
+                numSecondsTextBox.Text = "";
+                resultTextBox.Text = "";
+            }
+        }
+    }
+}
